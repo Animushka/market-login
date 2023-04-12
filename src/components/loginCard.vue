a83956449ba2a4e5140f1fd0777209af7a3ac369
sharafutdinov <asharafutdinov@tko-inform.ru>
2023-04-12 14:27:58 +0300

    add oauth

--- src/components/loginCard.vue
+++ src/components/loginCard.vue
@@ -12,6 +12,7 @@
 						</q-card-section>
 						<q-card-actions class="q-px-md">
 							<q-btn class="full-width" color="green" label="Login" size="lg" unelevated/>
+              <q-btn class="full-width q-mt-lg"  color="primary" label="OAuth2.1" size="lg" unelevated @click="oauth2"/>
 						</q-card-actions>
 					</q-card>
 				</div>
@@ -21,10 +22,51 @@
 
 <script lang="ts" setup>
 
-import { ref } from "vue";
+import { computed, onBeforeMount, ref } from "vue";
+import { useRoute } from 'vue-router';
+import axios from 'axios';
 
 const login = ref('')
 const password = ref('')
+
+const route = useRoute();
+
+const code = computed(() => {
+  var regExpMatchArray = route.fullPath.match(/code=(.*)/);
+  if (regExpMatchArray) {
+    return regExpMatchArray[1];
+  }
+
+  return undefined;
+});
+
+onBeforeMount(() => {
+  if (code.value) {
+    getBearerToken();
+  }
+})
+
+
+const url = 'http://localhost:9000/oauth2/authorize?response_type=code&client_id=sandwich-admin-client&scope=writeIngredients deleteIngredients&redirect_uri=http://127.0.0.1:9001/login'
+
+const oauth2 = () => {
+  window.open(url, "_self");
+}
+
+const getBearerToken = () => {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'authorization_code');
+  params.append('code', code.value);
+  params.append('redirect_uri', 'http://127.0.0.1:9001/login');
+  axios.post('http://127.0.0.1:9000/oauth2/token', params, {
+    auth: {
+      username: 'sandwich-admin-client',
+      password: 'secret',
+    }
+  }).then(response => localStorage.setItem('TOKEN', JSON.stringify(response.data))).catch(error => {
+    console.log(error);
+  })
+}
 
 </script>
 
